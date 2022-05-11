54fc15e981c92d6867e5d0ccd8d852011c014e5d
rluver <skyd0608@gmail.com>
2022-05-11 14:49:08 +0900

    implement cnn layer

--- tensorflow/layers/layers.py
+++ tensorflow/layers/layers.py
@@ -3,11 +3,18 @@ from ...config.config import ModelConfig
 import tensorflow
 from tensorflow.keras.layers import Layer, Dense, GRU, Conv1D, Dropout
 
-
-class Layer(Layer):
+    '''
+    h: desirable horizon
+    n: the variable dimension
+    X: n X T
+    w: time dimension kernel size
+    d_c: the number of filters
+    p: the number of hidden cells skipped through
+    h_t^R: hidden state at t
+    h_(t-p+1): hiddenstate of recurrent-skip component from t - p + 1
+    '''
     def __init__(
         self, 
-        config=ModelConfig,
         **kwargs):
         self.config = ModelConfig
         super(Layer, self).__init__(self, config=kwargs)
@@ -17,14 +24,21 @@ class Layer(Layer):
         ar_output = GRU(units=self.config.ar_units)(x)
         ar_output = Dropout(rates=self.config.dropout_rate)(ar_output)
 
+        # expand dim
+        # change axis (time, feature) -> (feature, time)
+        x = Permute((2, 1))(x)
+        x_expanded_dim = tf.expand_dims(x, -1)
+
         # Convolutional Layer
-        conv_output = Conv1D(
+        conv_output = Conv2D(
             filters=self.config.num_filters, 
-            kernel_size=self.config.kernel_size,
+            kernel_size=(x.shape[1], self.config.window),
             padding=self.config.padding,
             activation=self.config.activation
-            ) # num_filters x T
-        conv_output = Dropout(rates=self.config.dropout_rate)(conv_output)
+            )(x_expanded_dim)  # 1 x T x d_c
+        conv_output = tf.reshape(conv_output, shape=(-1, conv_output.shape[-2], conv_output.shape[-1]))  # T_c x d_c
+        conv_output = tf.Permute((2, 1))(conv_output)   # d_c x T
+        conv_output = Dropout(rate=self.config.dropout_rate)(conv_output)
 
         # Recurrent and Recurrent-skip layer
         recurrent_output = GRU(units=self.config.recurrent_units)(conv_output)
